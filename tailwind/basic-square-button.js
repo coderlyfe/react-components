f43d59074b7a62d5fa43e91ae2fc8e67c39bc4ad
Lamar Marshall <lmarshall53@verizon.net>
2021-11-24 00:39:01 -0500

    updated components

--- tailwind/basic-square-button.js
+++ tailwind/basic-square-button.js
@@ -1,5 +1,5 @@
 export let BasicSquareButton = (props) => (
-    <button type="button" class=" py-2 px-2  bg-green-600 hover:bg-green-700 focus:ring-green-500 focus:ring-offset-green-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 ">
+    <button type="button" className={` py-2 px-2  bg-${props.color ? props.color : "green"}-600 hover:bg-${props.color ? props.color : "green"}-700 focus:ring-${props.color ? props.color : "green"}-500 focus:ring-offset-${props.color ? props.color : "green"}-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 `}>
         {props.title ? props.title : "place holder"}
     </button>
 )
